a1f14f28b832df5e970332d89682b2b3563d6f3b
abankalarm <bamalkaranbamal@gmail.com>
2021-10-15 17:12:57 +0530

    oAuth2 google

--- app/home/routes.py
+++ app/home/routes.py
@@ -7,7 +7,7 @@ import os
 import pathlib
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, session, jsonify
 from flask_login import login_required, current_user
 from app import login_manager, db
 from jinja2 import TemplateNotFound
@@ -27,6 +27,12 @@ import datetime
 import json
 import sqlite3
 import requests
+import requests
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import httplib2
+
 pysqldf = lambda q: sqldf(q, globals())
 
 dfActivity = pd.read_csv("./CSVs/EmployeeActivity.csv")
@@ -692,3 +698,225 @@ def profile(template):
     #recom,Graph=getRecommendations(r1,r2,r3,r4,r5)
     #print(recom)
     return render_template('one-skill.html', segment = get_segment(request), resources=CDN.render(), allData = allDataSupplied)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# GOOGLE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# This variable specifies the name of a file that contains the OAuth 2.0
+# information for this application, including its client_id and client_secret.
+CLIENT_SECRETS_FILE = "client_secret.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the
+# authenticated user's account and requires requests to use an SSL connection.
+SCOPES = ['https://www.googleapis.com/auth/fitness.body.read','https://www.googleapis.com/auth/fitness.activity.read','https://www.googleapis.com/auth/fitness.location.read','https://www.googleapis.com/auth/fitness.oxygen_saturation.read','https://www.googleapis.com/auth/fitness.reproductive_health.read','https://www.googleapis.com/auth/fitness.sleep.read','https://www.googleapis.com/auth/fitness.activity.read','https://www.googleapis.com/auth/calendar','https://www.googleapis.com/auth/calendar.events','https://www.googleapis.com/auth/calendar.events.readonly','https://www.googleapis.com/auth/calendar.readonly','https://www.googleapis.com/auth/calendar.settings.readonly']
+API_SERVICE_NAME = 'Fitness API'
+API_VERSION = 'v2'
+
+
+@blueprint.route('/indextable')
+def print_index_table1():
+    return print_index_table()
+
+
+@blueprint.route('/test')
+def test_api_request():
+    
+    #     return redirect('authorize')
+
+    # # Load credentials from the session.
+    # credentials = google.oauth2.credentials.Credentials(
+    #     **session['credentials'])
+
+    # drive = googleapiclient.discovery.build(
+    #     API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+    # files = drive.files().list().execute()
+
+    # # Save credentials back to session in case access token was refreshed.
+    # # ACTION ITEM: In a production app, you likely want to save these
+    # #              credentials in a persistent database instead.
+    # session['credentials'] = credentials_to_dict(credentials)
+
+    # return jsonify(**files)
+
+    calendars = []
+    credentials = google.oauth2.credentials.Credentials(**session['credentials'])
+    if 'credentials' not in session:
+        credentials = google.oauth2.credentials.Credentials(**session['credentials'])
+    service = googleapiclient.discovery.build('calendar', 'v3', credentials=credentials)
+    page_token = None
+    while True:
+      calendar_list = service.calendarList().list(pageToken=page_token).execute()
+      for calendar_list_entry in calendar_list['items']:
+        calendars.append({"name": calendar_list_entry['summary'], "id": calendar_list_entry['id']})
+      page_token = calendar_list.get('nextPageToken')
+      if not page_token:
+        break
+    return jsonify("calendarReturn", {"data": calendars})
+
+
+@blueprint.route('/authorize')
+def authorize():
+    # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+    # The URI created here must exactly match one of the authorized redirect URIs
+    # for the OAuth 2.0 client, which you configured in the API Console. If this
+    # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
+    # error.
+    flow.redirect_uri = url_for('.oauth2callback', _external=True)
+    print(flow.redirect_uri)
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        state = 'dummy',
+        # Enable incremental authorization. Recommended as a best practice.
+        include_granted_scopes='true')
+
+    # Store the state so the callback can verify the auth server response.
+    session['state'] = state
+    print(authorization_url)
+    return redirect(authorization_url)
+
+
+@blueprint.route('/oauth2callback')
+def oauth2callback():
+    # Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    state = session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = url_for('.oauth2callback', _external=True)
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    # Store credentials in the session.
+    # ACTION ITEM: In a production app, you likely want to save these
+    #              credentials in a persistent database instead.
+    credentials = flow.credentials
+    session['credentials'] = credentials_to_dict(credentials)
+
+    return redirect(url_for('.test_api_request'))
+
+
+@blueprint.route('/revoke')
+def revoke():
+    if 'credentials' not in session:
+        return ('You need to <a href="/authorize">authorize</a> before ' +
+                'testing the code to revoke credentials.')
+
+    credentials = google.oauth2.credentials.Credentials(
+        **session['credentials'])
+
+    revoke = requests.post('https://oauth2.googleapis.com/revoke',
+        params={'token': credentials.token},
+        headers = {'content-type': 'application/x-www-form-urlencoded'})
+
+    status_code = getattr(revoke, 'status_code')
+    if status_code == 200:
+        return('Credentials successfully revoked.' + print_index_table())
+    else:
+        return('An error occurred.' + print_index_table())
+
+
+@blueprint.route('/clear')
+def clear_credentials():
+    if 'credentials' in session:
+        del session['credentials']
+    return ('Credentials have been cleared.<br><br>' +
+            print_index_table())
+
+
+def credentials_to_dict(credentials):
+    return {'token': credentials.token,
+            'refresh_token': credentials.refresh_token,
+            'token_uri': credentials.token_uri,
+            'client_id': credentials.client_id,
+            'client_secret': credentials.client_secret,
+            'scopes': credentials.scopes}
+
+def print_index_table():
+    return ('<table>' +
+            '<tr><td><a href="/test">Test an API request</a></td>' +
+            '<td>Submit an API request and see a formatted JSON response. ' +
+            '    Go through the authorization flow if there are no stored ' +
+            '    credentials for the user.</td></tr>' +
+            '<tr><td><a href="/authorize">Test the auth flow directly</a></td>' +
+            '<td>Go directly to the authorization flow. If there are stored ' +
+            '    credentials, you still might not be prompted to reauthorize ' +
+            '    the application.</td></tr>' +
+            '<tr><td><a href="/revoke">Revoke current credentials</a></td>' +
+            '<td>Revoke the access token associated with the current user ' +
+            '    session. After revoking credentials, if you go to the test ' +
+            '    page, you should see an <code>invalid_grant</code> error.' +
+            '</td></tr>' +
+            '<tr><td><a href="/clear">Clear Flask session credentials</a></td>' +
+            '<td>Clear the access token currently stored in the user session. ' +
+            '    After clearing the token, if you <a href="/test">test the ' +
+            '    API request</a> again, you should go back to the auth flow.' +
+            '</td></tr></table>')
